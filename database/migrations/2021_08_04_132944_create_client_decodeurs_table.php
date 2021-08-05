7ae889cb8c0af3cde11d9f9955a1ab348e715f8a
chakra13 <chatrynguemen@gmail.com>
2021-08-05 09:40:34 +0100

    nouvelle migration

---
+++ database/migrations/2021_08_04_132944_create_client_decodeurs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientDecodeursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_decodeurs',function (Blueprint $table) {
+            $table->id();
+            $table->date("date_reabonnement");
+            $table->timestamps();
+            $table->unsignedBigInteger('id_client');
+            $table->foreign('id_client')
+                ->references('id_client')
+                ->on('clients')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->unsignedBigInteger('id_decodeur');
+            $table->foreign('id_decodeur')
+                ->references('id_decodeur')
+                ->on('decodeurs')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->integer("id_user");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_decodeurs');
+    }
+}
